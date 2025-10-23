71ff09ee2365fc2907a480e727fcf57c9339220b
sambaraks652 <sambaraks652@gmail.com>
2025-10-23 23:25:36 +0300

    finished writing google-auth code in views

--- AuthenticationProject/Core/views.py
+++ AuthenticationProject/Core/views.py
@@ -1,16 +1,74 @@
+from lib2to3.pgen2 import token
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.conf import settings
 from google.auth.transport import requests as google_requests
 from google.oauth2 import id_token
-from .models import CustomUser
+from .models import User
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
 
 # Create your views here.
+
+@api_view(["POST"])
+def google_auth(request):
+
+
+    token = request.data.get("token")
+    if not token:
+      return Response({"error": "Token not provided","status":False}, status=status.HTTP_400_BAD_REQUEST)
+    
+    try:
+       
+       id_info = id_token.verify_oauth2_token(
+          token, 
+          google_requests.Request(), 
+          settings.GOOGLE_OAUTH_CLIENT_ID
+        )
+       print(id_info)
+       
+
+       email = id_info['email']
+       first_name = id_info.get('given_name', '')
+       last_name = id_info.get('family_name', '')
+       profile_pic_url = id_info.get('picture', '')
+
+       user, created = CustomUser.objects.get_or_create(email=email)
+       if created:
+        user.set_unusable_password()
+        user.first_name = first_name
+        user.last_name = last_name
+        user.registration_method = 'google'
+        user.save()
+
+       else:
+         if user.registration_method != 'google':
+          return Response({
+            "error": "User needs to sign in through email",
+            "status": False
+        }, status=status.HTTP_403_FORBIDDEN)
+         
+         refresh = RefreshToken.for_user(user)
+         return Response(
+         {
+            "tokens": {
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+          }
+        "status": True
+       },
+      status=status.HTTP_200_OK
+  )
+
+       
+
+    except ValueError:
+        return Response({"error": "Invalid token","status":False}, status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(['GET'])
 def Home(request):
     return Response('Hey Gys Just written my first Django rest Api in views file')
